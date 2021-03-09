f13caa259e0b1152cfad2891f320656d4da2ed1a
bleachbyte <jonathan.pirro@gsa.gov>
2021-03-08 21:41:53 -0600

    fixing gemfile

--- Gemfile.rb
+++ Gemfile.rb
@@ -1,9 +1,3 @@
 source "https://rubygems.org"
-ruby RUBY_VERSION
-
-gem "jekyll", "3.8.5"
-
-group :jekyll_plugins do
-   gem "jekyll-feed", "~> 0.6"
-   gem "jekyll-paginate-v2"
-end
+gem 'jekyll'
+gem 'jekyll-paginate'
